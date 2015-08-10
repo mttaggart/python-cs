6a758dbfed22381556d9f2df6132cfdeb7dac36e
mttaggart <michael.t.taggart@gmail.com>
2015-08-09 23:55:48 -0700

    Refactor out functions from rps1

--- RPS/rps-1.py
+++ RPS/rps-1.py
@@ -6,30 +6,34 @@
 
 import random # We need thr random module for the computer to play
 
+# This dictionary relates a choice to what it defeats for easy comparison later.
 beats = {
     "rock":"scissors",
     "paper":"rock",
     "scissors":"paper"
 }
 
+# Now we make an easy-to-use list of choices from the beats
 choices = list(beats.keys())
 
-def get_computer_choice():
-    return random.choice(choices)
+# Get the player choice from the input command.
+# The lower() is used to correct for people typing with capitals.
+# The strip() removes any trailing space that might mess us up.
+player_choice = input("Rock, paper, or scissors?").lower().strip()
 
-def compare(player_choice,computer_choice):
-    """
-    Return player victory. True if player wins, False if computer wins.
-    """
-    return beats[player_choice] == computer_choice
-
-player_choice = input("Rock, paper, or scissors?").lower()
-
+# Finally, time to compare! But we have to account for cheaters/mistakes.
 if player_choice not in choices:
     print("You must choose r, p, or s!")
 else:
-    computer_choice = get_computer_choice()
-    if compare(player_choice,computer_choice):
+    # Here we make the computer choose, then compare the two.
+    computer_choice = random.choice(choices)
+    if beats[player_choice] == computer_choice
         print("You win!")
     else:
         print("You lose")
+
+"""
+This program works, but it's quite limited. Probably its biggest limitation
+is that it will only run once. It also doesn't keep score—why would it, since
+it only runs once? Our next version of the game will make it more user-friendly.
+"""
